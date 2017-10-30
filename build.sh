bdbbefce0fc78ebf14cdfb961f5589427bd60584
Kumar Satyam <kr.satyam.ind@gmail.com>
2017-10-30 13:42:28 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,11 @@
-sudo apt-get install python3
+sudo apt-get install -y python3
+sudo apt-get install -y python3-venv
+python3 -m venv myenvsat
+source myenvsat/bin/activate
 
-python3 -m venv ENV
-. ENV/bin/activate
-
-sudo apt-get install python-pip python-dev build-essential 
-sudo pip install --upgrade pip 
-sudo pip install --upgrade virtualenv 
+sudo apt-get install -y python-pip python-dev build-essential 
+sudo pip install -y --upgrade pip 
+sudo pip install -y --upgrade virtualenv 
 pip install -r requirements.txt
 sudo chown -R $USER: .
-python proxy-net.py
-
-
+python3 proxy-net.py
