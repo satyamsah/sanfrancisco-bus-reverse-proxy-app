32ad6815a009bc155880b7cea76867451e6c44f4
Kumar Satyam <kr.satyam.ind@gmail.com>
2017-10-30 13:50:45 -0400

    Add files via upload

---
+++ build-from-docker.sh
@@ -0,0 +1,5 @@
+sudo apt-get install -y docker
+sudo apt-get install -y curl
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo docker-compose up
